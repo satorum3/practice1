7b3c9b9fc2543a5fe2ed79ab0a396f653895906c
松本智 <satorum@macbook-air.local>
2016-02-20 09:34:24 +0900

    Initial Commit

---
+++ practice1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice1
+//
+//  Created by 松本智 on 2016/02/20.
+//  Copyright © 2016年 satorum. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
